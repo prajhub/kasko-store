770f1c607a8200e23034448efc180f688f13267b
Prajwal Magar <pzlmagar@gmail.com>
2023-02-11 13:42:15 +0545

    Started Landing Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
+import LandingPage from './Pages/LandingPage'
 import LoginPage from './Pages/LoginPage'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 function App() {
  
   return (
     <>
-      <LoginPage/>
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+        </Routes>
+      </div>
+    </Router>
+      
     </>
   )
 }
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import NavBar from '../Components/NavBar'
+
+const LandingPage = () => {
+  return (
+    <>
+    
+        <NavBar/>
+        <Hero/>
+    
+    </>
+  )
+}
+
+export default LandingPage
